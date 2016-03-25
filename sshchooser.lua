34837b8450c8b345f950c2194a02c44fc7fb3ab3
Etan Reisner <deryni@unreliablesource.net>
2016-03-24 21:43:36 -0400

    Add support for a user-configuration file.

--- sshchooser.lua
+++ sshchooser.lua
@@ -2,6 +2,26 @@
 local sshmods = {"alt", "ctrl"}
 local sshkey = "p"
 
+local HOME = HOME or os.getenv("HOME")
+
+-- Load user configuration.
+local cfgfile = io.open(HOME.."/.hammerspoon/sshchooser.cfg")
+if cfgfile then
+    local env = {}
+    local chunk, err = load(cfgfile:read("*a"), "sshchooser.cfg", "t", env)
+    cfgfile:close()
+    if chunk then
+        chunk()
+
+        if env.sshkey then
+            sshkey = env.sshkey
+        end
+        if env.sshmods then
+            sshmods = env.sshmods
+        end
+    end
+end
+
 local function ssh_get_hosts()
     local ssh_hosts = {}
 end
